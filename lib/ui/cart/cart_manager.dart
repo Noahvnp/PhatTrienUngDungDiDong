2f500382708725c2ad7a9e3301594aba2cd9bae0
Noahvnp <norahpvn@gmail.com>
2022-10-27 00:21:14 +0700

    Bo sung code b3 : Quan ly trang thai voi provider - CartManager & OrdersManager

--- lib/ui/cart/cart_manager.dart
+++ lib/ui/cart/cart_manager.dart
@@ -4,14 +4,14 @@ import '../../models/cart_item.dart';
 import '../../models/product.dart';
 
 class CartManager with ChangeNotifier {
-  Map<String, CartItem> _items = {
-    'p1': CartItem(
-      id: 'c1',
-      title: 'Red Shirt',
-      price: 29.99,
-      quantity: 2,
-    ),
-  };
+  Map<String, CartItem> _items = {};
+  //   'p1': CartItem(
+  //     id: 'c1',
+  //     title: 'Red Shirt',
+  //     price: 29.99,
+  //     quantity: 2,
+  //   ),
+  // };
 
   int get productCount {
     return _items.length;
